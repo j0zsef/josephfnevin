80e065e16b6c3d78252866835ad9bbb7dc4a6bce
Joe Nevin <jnevin@enova.com>
2024-12-16 11:17:33 -0600

    Fixed error with localStorage not defined in Theme context.

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -10,20 +10,31 @@ import {
 
 interface ThemeContextType {
     theme: string;
-    setTheme: Dispatch<SetStateAction<string>>;
+    setTheme: Dispatch<SetStateAction<string | null>>;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<string>(() => {
-    return localStorage.getItem('theme') || 'light';
-  });
+  const [theme, setTheme] = useState<string | null>(null);
+
+  // run once on mount
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme') || 'light';
+    setTheme(storedTheme);
+    document.documentElement.setAttribute('data-bs-theme', storedTheme);
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('theme', theme);
-    document.documentElement.setAttribute('data-bs-theme', theme);
+    if (theme) {
+      localStorage.setItem('theme', theme);
+      document.documentElement.setAttribute('data-bs-theme', theme);
+    }
   }, [theme]);
+
+  if (theme === null) {
+    return null;
+  }
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
