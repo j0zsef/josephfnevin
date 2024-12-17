41f0c58e34db8d720bdca962a245c391f48f277c
Joe Nevin <jnevin@enova.com>
2024-12-17 13:28:01 -0600

    Fixing some styles. Trying google analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navigation from '@/components/Navigation';
 import { ThemeProvider } from '@/context/ThemeProvider';
 import { Container, Row } from 'react-bootstrap';
 import React from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: 'Joseph Nevin',
@@ -17,6 +18,7 @@ type RootLayoutProps = {
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-2B0Z71ZRP2" />
       <body>
         <ThemeProvider>
           <Container className="root-container">
